b267fba35e3d9c15ad1e0e9b5152c219ec82dd55
John Karima <karimajohn24@gmail.com>
2020-08-29 19:44:19 +0300

    scrapped buggy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { GhSearchComponent } from './gh-search/gh-search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFormComponent,
-    GhSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
